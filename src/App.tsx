f1ca0a37ab5f636fe9f506704875e250bd2a0bd8
KatherinaKulinich <kulinichcatherina@gmail.com>
2024-08-24 19:54:40 +0300

    forms done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import BasicForm from './components/forms/BasicForm';
-// import FormikForm from './components/forms/FormikForm';
-// import ReactHookForm from './components/forms/ReactHookForm';
 import RefForm from './components/forms/RefForm';
 
 const App = () => {
     return (
         <div className='min-h-screen w-full bg-[url("/src/assets/bg.jpg")] bg-cover'>
-            <div className='mx-44 flex h-full flex-wrap justify-center gap-8 p-8'>
+            <div className='mx-44 flex min-h-screen flex-wrap items-center justify-center gap-8 p-8'>
                 <div className=''>
                     <BasicForm />
                 </div>
                 <div className=''>
                     <RefForm />
                 </div>
-                {/* <div className=''>
-                    <FormikForm />
-                </div>
-                <div className=''>
-                    <ReactHookForm />
-                </div> */}
             </div>
         </div>
     );
